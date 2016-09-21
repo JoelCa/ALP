6eb8ec40e1ea1c00cd2db29103105d7fa86ea5e1
Terminal 9 <tc9@servidor-terminales-01.(none)>
2016-09-21 18:05:43 -0300

    mauro

--- interactivo/Main.hs
+++ interactivo/Main.hs
@@ -16,6 +16,10 @@ import Data.List (findIndex, elemIndex)
 
 type ProverInputState a = InputT (StateT ProverState IO) a
 
+instance MonadState ProverState ProverInputState where
+     get = lift . get
+     put = lift . put
+
 initialT' = [("intro_and", intro_and),
              ("elim_and", elim_and),
              ("intro_or1", intro_or1),

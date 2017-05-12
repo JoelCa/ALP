a612f2fcdb159b9cc137a91506d50d81a9739161
JoelCa <jxcata@gmail.com>
2017-05-11 21:19:34 -0300

    empty terms

---
+++ interactivo/EmptyTerms.hs
@@ -0,0 +1,43 @@
+module EmptyTerms where
+
+import Common
+
+-- Funciones que operan sobre los lambda términos con aujeros.
+
+simplifyTypeInTerm :: (Type, TType) -> [SpecialTerm] -> [SpecialTerm]
+simplifyTypeInTerm ty (TypeH (HTe h) : ts) = simplify (h ty) ts
+simplifyTypeInTerm ty (TypeH (HTy h) : ts) = (TypeH $ h ty) : ts
+
+simplify :: Term -> [SpecialTerm] -> [SpecialTerm]
+simplify t [HoleT et] = [Term $ et t]
+simplify t ((DoubleHoleT et):ts) = (HoleT $ et t):ts
+simplify t ((HoleT et):(DoubleHoleT et'):ts) = (HoleT $ (et' . et) t):ts
+
+addHT :: (Term->Term) -> [SpecialTerm] -> [SpecialTerm]
+addHT et ((HoleT et'):ts) = (HoleT $ et' . et):ts
+addHT et ((DoubleHoleT et'):ts) = (DoubleHoleT $ et' . et):ts
+
+addDHT :: (Term->Term->Term) -> [SpecialTerm] -> [SpecialTerm]
+addDHT et ((HoleT et'):ts) = (DoubleHoleT $ (\f -> et' . f) . et):ts
+addDHT et ((DoubleHoleT et'):ts) = (DoubleHoleT et):(DoubleHoleT et'):ts
+
+-- addTypeHole añade al lambda término de su primer arg., una instancia de tipo "vacia".
+-- Retorna el TypeHole con la instacia "vacia".
+addTypeHole :: SpecialTerm -> TypeHole
+addTypeHole (Term te) = HTe $ \y -> te :!: y
+addTypeHole (TypeH hte) = HTy $ \y -> addTypeTerm hte y
+addTypeHole _ = error "error: newTypeHole, no debería pasar."
+
+-- addTypeTermST añade al lambda término de su primer arg., una instancia de tipo "vacia".
+-- Retorna el SpecialTerm con la instacia "vacia".
+addTypeTermST :: SpecialTerm -> (Type, TType) -> SpecialTerm
+addTypeTermST (Term te) x = Term $ te :!: x
+addTypeTermST (TypeH hte) x = TypeH $ addTypeTerm hte x
+addTypeTermST _ _ = error "error: addTypeTerm, no debería pasar."
+
+-- addTypeTerm añade al lambda término de su primer arg. (desempaquetando el TypeHole), una instancia de tipo,
+-- con el tipo de su segundo argumento. Retorna el TypeHole con esta nueva instancia de tipo, respetando la misma
+-- estructura del TypeHole de su primer arg.
+addTypeTerm :: TypeHole -> (Type, TType) -> TypeHole
+addTypeTerm (HTe h) x = HTe $ \y -> h y :!: x
+addTypeTerm (HTy h) x = HTy $ \y -> addTypeTerm (h y) x
